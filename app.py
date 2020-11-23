a48889be0c8fc4d8211977619986e72ea207d636
greg <gdry2090@gmail.com>
2020-11-23 12:53:47 -0500

    basic flask web app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'hello world'
+
+@app.route('/home')
+def home_page():
+    return str(random.randint(1,10))
+
+if __name__=="__main__":
+    app.run()
